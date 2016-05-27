a370d048cc3aa31a4726757382afaf96900034a5
Hainan Xu <hxu@Hainans-MacBook-Pro.local>
2016-05-27 23:57:24 +0800

    feats

--- scripts/generate-pos-features.sh
+++ scripts/generate-pos-features.sh
@@ -12,5 +12,5 @@ set -x
 
 for lang in $input $output; do
   python $ROOT/scripts/tags-stats.py $ref_corpus.$lang - | sort -n -k2 -r | awk '{print$1}' | head -n $pos_num > $working/$id/step-3/tagged/pos.list.$lang
-  python $ROOT/scripts/collect-tags.py $working/$id/step-3/tagged/pos.list.$lang $corpus.$lang - | awk '{for(i=2;i<=NF;i+=2)printf("%s ",$i);print""}' > $working/$id/step-3/$file.$lang.pos.count
+  python $ROOT/scripts/collect-tags.py $working/$id/step-3/tagged/pos.list.$lang $corpus.$lang - | awk '{for(i=2;i<=NF;i+=2)printf("%s ",$i);print""}' > $working/$id/step-3/tagged/$file.$lang.pos.count
 done

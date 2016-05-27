fd7b855a0636e8eafc37383cb226828c30094a13
Hainan Xu <hxu@Hainans-MacBook-Pro.local>
2016-05-27 22:13:06 +0800

    pos tags

--- scripts/3.sh
+++ scripts/3.sh
@@ -15,11 +15,19 @@ mkdir -p $working/$id/step-3/tagged/
 if [ $pos_feat = true ]; then
   echo "[step-3] running the Stanford tagger to generate PoS features"
 
-  echo $ROOT/scripts/tag-pos.sh $config $train $input $working/$id/step-3/tagged/good.$input
-  $ROOT/scripts/tag-pos.sh $config $train $input $working/$id/step-3/tagged/good.$input
-  $ROOT/scripts/tag-pos.sh $config $train $output $working/$id/step-3/tagged/good.$output
+# TODO; restore this
+#  $ROOT/scripts/tag-pos.sh $config $train $input $working/$id/step-3/tagged/good.$input
+#  $ROOT/scripts/tag-pos.sh $config $train $output $working/$id/step-3/tagged/good.$output
+#
+#  $ROOT/scripts/tag-pos.sh $config $test $input $working/$id/step-3/tagged/bad.$input
+#  $ROOT/scripts/tag-pos.sh $config $test $output $working/$id/step-3/tagged/bad.$output
 
-  $ROOT/scripts/tag-pos.sh $config $test $input $working/$id/step-3/tagged/bad.$input
-  $ROOT/scripts/tag-pos.sh $config $test $output $working/$id/step-3/tagged/bad.$output
+
+  cat $working/$id/step-3/tagged/good.$input | head -n $pos_sample > $working/$id/step-3/tagged/good.sample.$input
+  cat $working/$id/step-3/tagged/good.$output | head -n $pos_sample > $working/$id/step-3/tagged/good.sample.$output
+
+  $ROOT/scripts/generate-pos-features.sh $config $working/$id/step-3/tagged/good.sample $working/$id/step-3/tagged/good
+  $ROOT/scripts/generate-pos-features.sh $config $working/$id/step-3/tagged/good.sample $working/$id/step-3/tagged/bad
 
 fi
+

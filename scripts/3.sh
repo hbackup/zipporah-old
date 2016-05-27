efac9047c0756d70db17c67370efd022fc2d0b58
Hainan Xu <hxu@Hainans-MacBook-Pro.local>
2016-05-27 23:31:29 +0800

    feats

--- scripts/3.sh
+++ scripts/3.sh
@@ -13,7 +13,10 @@ mkdir -p $working/$id/step-3
 mkdir -p $working/$id/step-3/feats
 feats=$working/$id/step-3/feats
 
-if [ $pos_feat = true ]; then
+good_string=
+bad_string=
+
+if [ $pos_feat = true ] && [ ! -f $working/$id/step-3/feats/bad.pos ]; then
   mkdir -p $working/$id/step-3/tagged/
   echo "[step-3] running the Stanford tagger to generate PoS features"
 
@@ -35,18 +38,26 @@ if [ $pos_feat = true ]; then
     done
   done
 
-# TODO change to ratio
   paste $working/$id/step-3/tagged/good.$input.pos.count.ratio $working/$id/step-3/tagged/good.$output.pos.count.ratio > $working/$id/step-3/feats/good.pos
   paste $working/$id/step-3/tagged/bad.$input.pos.count.ratio $working/$id/step-3/tagged/bad.$output.pos.count.ratio > $working/$id/step-3/feats/bad.pos
-
 fi
 
-if [ $bow_feat = true ]; then
+if [ $pos_feat = true ]; then
+  good_string="$good_string $feats/good.pos"
+  bad_string="$bad_string $feats/bad.pos"
+fi
+
+if [ $bow_feat = true ] && [ ! -f $working/$id/step-3/feats/bad.bow.e2f ]; then
   $ROOT/scripts/run-bow.sh $config $f2e $train.$input $train.$output > $working/$id/step-3/feats/good.bow.f2e
   $ROOT/scripts/run-bow.sh $config $e2f $train.$output $train.$input > $working/$id/step-3/feats/good.bow.e2f
 
   $ROOT/scripts/run-bow.sh $config $f2e $test.$input $train.$output > $working/$id/step-3/feats/bad.bow.f2e
   $ROOT/scripts/run-bow.sh $config $e2f $test.$output $train.$input > $working/$id/step-3/feats/bad.bow.e2f
+fi
+
+if [ $bow_feat = true ]; then
+  good_string="$good_string $feats/good.bow.f2e $feats/good.bow.e2f"
+  bad_string="$bad_string $feats/bad.bow.f2e $feats/bad.bow/e2f"
 fi
 
 if [ $length_feat = true ] || [ $length_ratio = true ]; then
@@ -61,7 +72,25 @@ if [ $length_feat = true ] || [ $length_ratio = true ]; then
   done
 fi
 
-if [ $non_word_agree = true ]; then
+if [ $length_feat = true ]; then
+  good_string="$good_string $feats/good.$input.length $feats/good.$output.length"
+  bad_string="$bad_string $feats/bad.$input.length $feats/bad.$output.length"
+fi
+
+if [ $length_ratio = true ]; then
+  good_string="$good_string $feats/good.length.ratio"
+  bad_string="$bad_string $feats/bad.length.ratio"
+fi
+
+if [ $non_word_agree = true ] && [ ! -f $feats/bad.agree ]; then
   python $ROOT/scripts/non-word-agreement.py $train.$input $train.$output > $feats/good.agree
   python $ROOT/scripts/non-word-agreement.py $test.$input $train.$output > $feats/bad.agree
 fi
+
+if [ $non_word_agree = true ]; then
+  good_string="$good_string $feats/good.agree"
+  bad_string="$bad_string $feats/bad.agree"
+fi
+
+paste $good_string > $feats/good.feats
+paste $bad_string > $feats/bad.feats

ffd51c85e7dae1dfabdd0cd6b9500be9e00a6b21
Hainan Xu <hxu@Hainans-MacBook-Pro.local>
2016-05-28 09:15:15 +0800

    gmm

--- scripts/4.sh
+++ scripts/4.sh
@@ -9,12 +9,21 @@ fi
 echo "[step-4] starts"
 
 mkdir -p $working/$id/step-4/
+mkdir -p $working/$id/step-4/feats
 mkdir -p $working/$id/step-4/gmm-file
-
 
 feats=$working/$id/step-3/feats
 
-$ROOT/scripts/cluster-gmm.sh $config $feats/good.feats $num_gauss $working/$id/step-4/gmm-file/params $working/$id/step-4/gmm-file
+for i in good bad; do
+  n=`wc -l $feats/$i.feats | awk '{print$1}'`
+  k=$gmm_sample_size
+
+  $ROOT/tools/get-rand-index $n $k > $working/$id/step-4/feats/$i.index
+  $ROOT/tools/get-lines $working/$id/step-4/feats/$i.index $feats/good.feats > $working/$id/step-4/feats/$i.feats
+
+  $ROOT/scripts/cluster-gmm.sh $config $feats/good.feats $num_gauss $working/$id/step-4/gmm-file/params $working/$id/step-4/gmm-file
+
+done
 
 touch $working/$id/step-4/.done
 

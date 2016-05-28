97996db9d547a03da51b1210ba32620e945e0424
Hainan Xu <hxu@a11.clsp.jhu.edu>
2016-05-28 00:22:56 -0400

    a bug fix in step 6

--- scripts/6.sh
+++ scripts/6.sh
@@ -15,7 +15,7 @@ cat $working/$id/step-5/score.good | awk '{print $3}' | grep . > $working/$id/st
 cat $working/$id/step-5/score.bad | awk '{print $3}' | grep . > $working/$id/step-6/score.bad
 
 # some sanity checks
-wc -l $working/$id/step-6/score.* $working/$id/step-3/bad.feats $working/$id/step-1/bad.clean.short*
+wc -l $working/$id/step-6/score.* $working/$id/step-3/feats/bad.feats $working/$id/step-1/bad.clean.short*
 
 
 
